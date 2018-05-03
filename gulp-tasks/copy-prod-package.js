ee402d6d2bc57aa9257ccd16f27252df25be8b4c
Enzo Volkmann <enzo@exportarts.io>
2018-05-03 10:21:02 +0200

    Removed Gulp

--- gulp-tasks/copy-prod-package.js
+++
@@ -1,7 +0,0 @@
-const rename = require('gulp-rename');
-
-module.exports = (gulp, callback) => {
-    return gulp.src('src/package.prod.json')
-        .pipe(rename('package.json'))
-        .pipe(gulp.dest('dist/'));
-};
